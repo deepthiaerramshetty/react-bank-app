bd9772e74ec36218ddad9ad0d509594c48777229
deepthiaerramshetty <deepthiaerramshetty@gmail.com>
2023-03-14 22:44:17 -0400

    Updates

---
+++ precache-manifest.8b4c281971159d0b7e5be97b7569bdb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46a4054454719d03666",
+    "url": "/react-bank-app/static/css/main.4bc3b9f9.chunk.css"
+  },
+  {
+    "revision": "c46a4054454719d03666",
+    "url": "/react-bank-app/static/js/main.c46a4054.chunk.js"
+  },
+  {
+    "revision": "35ffad1a0530a19d1c0c",
+    "url": "/react-bank-app/static/js/1.35ffad1a.chunk.js"
+  },
+  {
+    "revision": "7eee2f13a35ad2b8241b",
+    "url": "/react-bank-app/static/js/runtime~main.7eee2f13.js"
+  },
+  {
+    "revision": "60c3d7477c20ae53e76d0363aa516c54",
+    "url": "/react-bank-app/index.html"
+  }
+];
